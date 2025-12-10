68d640dc49e17e15b6184c7d9ace046608f8b62b
aclandmark <mark.osborne15@btinternet.com>
2025-12-10 16:02:09 +0000

    Create mini_OS_CA_display.c

---
+++ mini_OS_CA_display/mini_OS_CA_display/mini_OS_CA_display.c
@@ -0,0 +1,18 @@
+/*
+ * mini_OS_CA_display.c
+ *
+ * Created: 10/12/2025 15:44:42
+ * Author : maosb
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
