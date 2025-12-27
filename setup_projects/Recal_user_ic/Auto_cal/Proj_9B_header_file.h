faa7f43a4767b5dccd035e641147f9cf2f3a0bb7
aclandmark <mark.osborne15@btinternet.com>
2025-12-27 18:40:45 +0000

    Create Proj_9B_header_file.h

---
+++ setup_projects/Recal_user_ic/Auto_cal/Proj_9B_header_file.h
@@ -0,0 +1,27 @@
+
+
+#include "Resources_nano_projects/Header_files/Basic_HW_setup.h"
+#include "Resources_nano_projects/Subroutines/Basic_HW_plus_Timer.c"
+#include "Resources_nano_projects/I2C_Subroutines/I2C_subroutines_1.c"
+#include "Resources_nano_projects/I2C_Subroutines/I2C_subroutines_2.c"
+#include "Resources_nano_projects/Subroutines/Basic_PC_comms.c"
+#include "Resources_nano_projects/Header_files/I_O_from switches.h"	
+#include "Resources_nano_projects/Subroutines/Integers_to_from_PC.c"
+#include "Resources_nano_projects/I2C_Subroutines/I2C_slave_Rx_Tx.c"
+#include "Resources_nano_projects/I2C_Subroutines/I2C_diagnostic.c"
+#include "Resources_nano_projects/Subroutines/Random_and_prime_nos.c"
+
+
+
+#include "Resources_nano_projects/Subroutines/Data_to_from_EEPROMs.c"
+//#include "Resources_nano_projects/Subroutines/Random_and_prime_nos.c"
+
+
+
+void I2C_initiate_7_8125mS_ref(void);
+long emergency_cal(void);
+long compute_error(void);
+long Calibration_tuning(char);
+
+#define waiting_for_I2C_master_with_ISR \
+TWCR = (1 << TWEA) | (1 << TWEN) | (1 << TWIE);
