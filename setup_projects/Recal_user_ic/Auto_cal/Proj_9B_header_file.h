22eae57d4e3e5ad587e126b0098d9cc1f8f1dc99
aclandmark <mark.osborne15@btinternet.com>
2025-12-27 21:35:28 +0000

    Update Proj_9B_header_file.h

--- setup_projects/Recal_user_ic/Auto_cal/Proj_9B_header_file.h
+++ setup_projects/Recal_user_ic/Auto_cal/Proj_9B_header_file.h
@@ -1,27 +1,126 @@
 
 
-#include "Resources_nano_projects/Header_files/Basic_HW_setup.h"
-#include "Resources_nano_projects/Subroutines/Basic_HW_plus_Timer.c"
-#include "Resources_nano_projects/I2C_Subroutines/I2C_subroutines_1.c"
-#include "Resources_nano_projects/I2C_Subroutines/I2C_subroutines_2.c"
-#include "Resources_nano_projects/Subroutines/Basic_PC_comms.c"
-#include "Resources_nano_projects/Header_files/I_O_from switches.h"	
-#include "Resources_nano_projects/Subroutines/Integers_to_from_PC.c"
-#include "Resources_nano_projects/I2C_Subroutines/I2C_slave_Rx_Tx.c"
-#include "Resources_nano_projects/I2C_Subroutines/I2C_diagnostic.c"
-#include "Resources_nano_projects/Subroutines/Random_and_prime_nos.c"
+#include <avr/wdt.h>
+
+char watch_dog_reset = 0;
+char User_response;
+
+#define T0_delay_10ms 5,178
+#define T1_delay_100ms 3, 0x9E62
+#define T2_delay_10ms 7,178
 
 
 
-#include "Resources_nano_projects/Subroutines/Data_to_from_EEPROMs.c"
-//#include "Resources_nano_projects/Subroutines/Random_and_prime_nos.c"
+/*****************************************************************************/
+#define setup_HW_Arduino_IO \
+setup_watchdog;\
+set_up_I2C;\
+ADMUX |= (1 << REFS0);\
+set_up_switched_inputs;\
+Set_LED_ports;\
+Unused_I_O;\
+eeprom_write_byte((uint8_t*)(0x1FD),OSCCAL);\
+while (!(PIND & (1 << PD1)));\
+Timer_T0_10mS_delay_x_m(5);\
+OSC_CAL;\
+Serial.begin(115200);\
+while (!Serial);\
+sei();
 
 
 
-void I2C_initiate_7_8125mS_ref(void);
-long emergency_cal(void);
-long compute_error(void);
-long Calibration_tuning(char);
+/*****************************************************************************/
+#define setup_watchdog \
+if (MCUSR & (1 << WDRF))watch_dog_reset = 1;\
+wdr();\
+MCUSR &= ~(1<<WDRF);\
+WDTCSR |= (1 <<WDCE) | (1<< WDE);\
+WDTCSR = 0;
+
+#define wdr()  __asm__ __volatile__("wdr")
+
+#define SW_reset {wdt_enable(WDTO_30MS);while(1);}
+
+
+
+/*****************************************************************************/
+#define set_up_I2C \
+TWAR = 0x02;
+
+
+
+/*****************************************************************************/
+#define set_up_switched_inputs \
+MCUCR &= (~(1 << PUD));\
+DDRD &= (~((1 << PD2)|(1 << PD7)));\
+PORTD |= ((1 << PD2) | (1 << PD7));\
+DDRB &= (~(1 << PB6));\
+PORTB |= (1 << PB6);
+
+
+
+/*****************************************************************************/
+#define Unused_I_O \
+MCUCR &= (~(1 << PUD));\
+DDRB &= (~((1 << PB2)|(1 << PB7)));\
+DDRC &= (~((1 << PC0)|(1 << PC1)|(1 << PC2)));\
+DDRD &= (~((1 << PD3)|(1 << PD4)|(1 << PD5)|(1 << PD6)));\
+PORTB |= ((1 << PB2)|(1 << PB7));\
+PORTC |= ((1 << PC0)|(1 << PC1)|(1 << PC2));\
+PORTD |= ((1 << PD3)|(1 << PD4)|(1 << PD5)|(1 << PD6));
+
+
+
+/*****************************************************************************/
+#define Set_LED_ports   DDRB = (1 << DDB0) | (1 << DDB1);
+#define LEDs_on       PORTB |= (1 << PB0)|(1 << PB1);
+#define LEDs_off      PORTB &= (~((1 << PB0)|(1 << PB1)));
+#define LED_1_on      PORTB |= (1 << PB1);
+#define LED_1_off     PORTB &= (~( 1<< PB1)); 
+#define LED_2_off     PORTB &= (~(1 << PB0));
+#define LED_2_on      PORTB |= (1 << PB0);
+
+#define Toggle_LED_1 \
+if (PORTB & (1 << PB1)){LED_1_off;}\
+else {PORTB |= (1 << PB1);}
+
+
+
+/*****************************************************************************/
+#define OSC_CAL \
+if ((eeprom_read_byte((uint8_t*)0x1FE) > 0x0F)\
+&&  (eeprom_read_byte((uint8_t*)0x1FE) < 0xF0) && (eeprom_read_byte((uint8_t*)0x1FE)\
+== eeprom_read_byte((uint8_t*)0x1FF))) {OSCCAL = eeprom_read_byte((uint8_t*)0x1FE);}
+
+
+
+/*****************************************************************************/
+#define User_prompt_A \
+while(1){\
+do{Serial.write("R?    ");}  while((isCharavailable_A (50) == 0));\
+User_response = Serial.read();\
+if((User_response == 'R') || (User_response == 'r'))break;} Serial.write("\r\n");
+
+
+
+/*****************************************************************************/
+#define waiting_for_I2C_master \
+TWCR = (1 << TWEA) | (1 << TWEN);\
+while (!(TWCR & (1 << TWINT)));\
+TWDR;
+
+#define clear_I2C_interrupt \
+TWCR = (1 << TWINT);
 
 #define waiting_for_I2C_master_with_ISR \
 TWCR = (1 << TWEA) | (1 << TWEN) | (1 << TWIE);
+
+
+
+/*****************************************************************************/
+#include "Resources_user_device_cal\HW_timers.c"
+#include "Resources_user_device_cal\Basic_Rx_Tx_Arduino.c"
+#include "Resources_user_device_cal\I2C_slave_Rx_Tx.c"
+
+
+/******************************************************************************/
