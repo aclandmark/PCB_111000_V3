c743d4f12a3eeab9b380b73af9beaae81442b048
aclandmark <mark.osborne15@btinternet.com>
2026-01-08 17:47:47 +0000

    Update Random_and_prime_nos.c

--- 4_Resources_nano_projects/Subroutines/Random_and_prime_nos.c
+++ 4_Resources_nano_projects/Subroutines/Random_and_prime_nos.c
@@ -14,10 +14,10 @@ unsigned char PRN_8bit_GEN(void){
 unsigned int bit;
 unsigned char lfsr;
 
-lfsr = eeprom_read_byte((uint8_t*)(0x1FC));
+lfsr = eeprom_read_byte((uint8_t*)(0x3FC));
 bit = (( lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 4)) & 1;
 lfsr = (lfsr >> 1) | (bit << 7);
-eeprom_write_byte((uint8_t*)(0x1FC),lfsr);
+eeprom_write_byte((uint8_t*)(0x3FC),lfsr);
 return lfsr;}
 
 
@@ -28,8 +28,8 @@ unsigned char eep_offset = 0;																//The subroutine provides it for ev
 
 
 if((!(*PRN_counter)) && (!(start)))														//Only read the EEPROM the first time a program calls this subroutine
-{lfsr = (eeprom_read_byte((uint8_t*)0x1FC) << 8) + 
-eeprom_read_byte((uint8_t*)0x1FB);}										//Data saved to EEPROM survives resets including POR (power on reset)
+{lfsr = (eeprom_read_byte((uint8_t*)0x3FC) << 8) + 
+eeprom_read_byte((uint8_t*)0x3FB);}										//Data saved to EEPROM survives resets including POR (power on reset)
 
 else lfsr = start;																		//Use previous PRN value to generate the next one
 
@@ -39,8 +39,8 @@ lfsr = (lfsr >> 1) | (bit << 15);
 *PRN_counter += 1;	 
 if(*PRN_counter == 255)
 
-{eeprom_write_byte((uint8_t*)0x1FC,(lfsr>>8));							//Save every 16th PRN number to EEPROM
-eeprom_write_byte((uint8_t*)0x1FB, lfsr);									//This prevents the display from endlessly repeating
+{eeprom_write_byte((uint8_t*)0x3FC,(lfsr>>8));							//Save every 16th PRN number to EEPROM
+eeprom_write_byte((uint8_t*)0x3FB, lfsr);									//This prevents the display from endlessly repeating
 Toggle_LED_1;															//Note: Saving every one burns out the EEPROM location too quickly
 *PRN_counter = 0;}
 
