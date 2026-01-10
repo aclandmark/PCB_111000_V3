3d1ff8f0a77dfeec1e8cc3c5265aca1ab32dd05e
aclandmark <mark.osborne15@btinternet.com>
2026-01-10 12:01:45 +0000

    Create 4B_Rx_interrupt.ino

---
+++ 1_Arduino Projects/5_Interrupts/4_Rx_interrupts/4B_Rx_interrupt/4B_Rx_interrupt.ino
@@ -0,0 +1,23 @@
+
+/*
+ A crude attempt to implement a new line function without also
+ resetting the cursor to the start of the line 
+ */
+
+volatile char sym_counter;
+
+#include "4B_Rx_interrupt_header.h"
+
+
+//Type main here
+
+
+ISR(USART_RX_vect){
+Char_from_PC_Basic();
+newline_Basic();
+for (int m = 0; m <= sym_counter; m++)String_to_PC_Basic("  ");}
+
+
+
+
+/*******************************************************************/
