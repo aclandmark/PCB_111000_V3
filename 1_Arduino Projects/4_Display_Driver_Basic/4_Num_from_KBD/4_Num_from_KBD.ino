9abae52199c1385cdc54233e24976009912ec1c1
aclandmark <mark.osborne15@btinternet.com>
2026-01-10 12:00:39 +0000

    Create 4_Num_from_KBD.ino

---
+++ 1_Arduino Projects/4_Display_Driver_Basic/4_Num_from_KBD/4_Num_from_KBD.ino
@@ -0,0 +1,34 @@
+
+
+//Entering numbers from the keyboard and letting the mini-OS convert them to binary 
+
+#include "Receiver_Transmitter_header.h"
+
+int main (void){   
+  char num_string[12];
+  long num;
+  
+setup_HW;
+if(watch_dog_reset == 1)String_to_PC_Basic("\r\nAgain"); else
+String_to_PC_Basic("Enter number at keyboard");
+num = Int_KBD_to_display_A_Local(num_string);
+waitforkeypress_Basic();
+while(num){
+num /= 2;
+I2C_Tx_long(num);
+waitforkeypress_Basic();}
+SW_reset;
+return 1;}
+
+
+
+
+/************************************************************************/
+//Type subroutine 
+//long Int_KBD_to_display_A_Local(char *) here
+
+
+
+
+
+/***********************Subroutines*********************************/
