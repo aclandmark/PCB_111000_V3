e274d662e8399fcbde29e921f386776e0d39f86a
aclandmark <mark.osborne15@btinternet.com>
2026-01-10 11:58:54 +0000

    Create 2_Bin_to_String.ino

---
+++ 1_Arduino Projects/3_Receiver_Transmitter_Basic/2_Bin_to_String/2_Bin_to_String.ino
@@ -0,0 +1,23 @@
+
+//5 Projects that explore the conversion of numbers to strings
+//so that they can be printed out
+
+
+#include "Receiver_Transmitter_header.h"
+#include "Local_subroutines.c"
+
+//Five examples to experiment with.
+
+
+
+
+
+
+
+
+
+
+
+
+
+//*************************************************************************************************************************
