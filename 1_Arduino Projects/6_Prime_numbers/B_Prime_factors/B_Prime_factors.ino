6313a006dbbc4f52db915c970991e6641f519a04
aclandmark <mark.osborne15@btinternet.com>
2026-01-10 12:02:22 +0000

    Create B_Prime_factors.ino

---
+++ 1_Arduino Projects/6_Prime_numbers/B_Prime_factors/B_Prime_factors.ino
@@ -0,0 +1,37 @@
+
+/*
+ Calculates the prime factors of a number entered at the keyboard
+ */
+
+
+#include "Prime_factors_header.h"
+
+/************************************************************************************************************/
+//Type main routine here
+
+
+
+/***************************************************************************************************************************/
+unsigned long next_prime_number(unsigned long num){
+ 
+ while (!(prime_num_check(++num)));
+ return num;}
+
+
+/***************************************************************************************************************************/
+char prime_num_check(unsigned int Num)                        
+{   
+int i = 2;   
+int m;  
+ while(1){  
+if (i*i > Num)break;  
+m=0;
+while(1){
+if(i*(i+m) == Num)return 0;
+if(i*(i+m) > Num)break;
+m += 1;}  
+i += 1;}
+return 1;}
+
+
+/*************************************************************************************/
