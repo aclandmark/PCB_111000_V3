ddb84cf84f51fc76ef1863d14e828915966b87ff
aclandmark <mark.osborne15@btinternet.com>
2026-01-10 11:58:41 +0000

    Create 1_askii_code.ino

---
+++ 1_Arduino Projects/3_Receiver_Transmitter_Basic/1_askii_code/1_askii_code.ino
@@ -0,0 +1,24 @@
+
+//10 projects that explore
+          //Text strings
+          //Askii characters
+          //Downloading and printing text files
+          //Storing strings in working memory
+          //Upper to lower case conversion
+          //Printing .hex files with text in clear
+
+
+
+
+#include "Receiver_Transmitter_header.h"
+#include "Local_subroutines.c"
+
+
+//Ten simple examples to experiment with.
+
+
+
+
+
+
+//**************************************************************************************************************************************
