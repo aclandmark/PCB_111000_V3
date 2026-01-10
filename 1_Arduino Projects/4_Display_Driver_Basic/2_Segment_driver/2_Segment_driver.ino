8f8ad70fc1e1e43b6f931428b2f20e6f3c192c2a
aclandmark <mark.osborne15@btinternet.com>
2026-01-10 12:00:18 +0000

    Create 2_Segment_driver.ino

---
+++ 1_Arduino Projects/4_Display_Driver_Basic/2_Segment_driver/2_Segment_driver.ino
@@ -0,0 +1,22 @@
+
+/*
+ Almost identical to 1_Segment_driver
+ */
+
+
+
+
+#include "Segment_driver_header.h"
+
+
+#define message_1 "\r\nFor each digit press 0 to 7 then a to g for segments. Press x to reset display.\r\n"
+#define message_2 "\r\nAgain!"
+
+
+
+//Type main routine here
+
+
+
+
+/***************************************************************************************************************************/
