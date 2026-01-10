87fb1525894689fde86cc93950062b8e6d28c265
aclandmark <mark.osborne15@btinternet.com>
2026-01-10 11:59:18 +0000

    Create 4_Char_numbers.ino

---
+++ 1_Arduino Projects/3_Receiver_Transmitter_Basic/4_Char_numbers/4_Char_numbers.ino
@@ -0,0 +1,21 @@
+
+//A second look at binary to decimal conversion
+
+
+#include "Num_types_header_file.h"
+#include "Local_subroutines.c"
+
+
+//Type main routine here.
+ 
+
+
+
+
+
+
+
+
+
+
+/**************************************************************************************************/
