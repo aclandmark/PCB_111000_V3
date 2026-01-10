a9c2fd5ce84ab94468e552ef86f6857220847b61
aclandmark <mark.osborne15@btinternet.com>
2026-01-10 12:00:48 +0000

    Create 5_Moving_display.ino

---
+++ 1_Arduino Projects/4_Display_Driver_Basic/5_Moving_display/5_Moving_display.ino
@@ -0,0 +1,12 @@
+
+//Practice in using logic to generate a rotating display
+
+#include "Receiver_Transmitter_header.h"
+#include "Local_subroutines.c"
+
+
+
+//Type main routine here
+
+
+/************************************************************************/
