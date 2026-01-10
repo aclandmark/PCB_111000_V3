a9dba1888224f7dcfce0c923dd70bc332ec7ef6f
aclandmark <mark.osborne15@btinternet.com>
2026-01-10 11:58:02 +0000

    Create 5_SW_multiplication.ino

---
+++ 1_Arduino Projects/2_binary_and_bit_operations/5_SW_multiplication/5_SW_multiplication.ino
@@ -0,0 +1,29 @@
+/*
+ Binary multiplication is simply a process of shift and add
+ For details see
+https://www.geeksforgeeks.org/maths/binary-multiplication/
+
+
+Multiplication can also be achieved by adding the multiplicand to
+itself as many times as the multiplier, but this process is relatively slow.
+
+For example 2 x 3 is 2 + 2 + 2 = 6
+
+*/
+
+
+
+
+#include "SW_multiplication_header.h"
+#include "Local_subroutines.c"
+#include "KBD_to_display.c"
+
+
+//Type main routine here
+
+
+
+
+
+
+/*****************************************************************************************************************************************/
