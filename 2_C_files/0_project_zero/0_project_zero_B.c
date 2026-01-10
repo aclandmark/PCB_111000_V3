1dbdcd73279398a18cde0e98cba099fe2e3a882c
aclandmark <mark.osborne15@btinternet.com>
2026-01-10 18:28:03 +0000

    Update 0_project_zero_B.c

--- 2_C_files/0_project_zero/0_project_zero_B.c
+++ 2_C_files/0_project_zero/0_project_zero_B.c
@@ -19,11 +19,11 @@ int main (void)  	//Example 5
   PRN = (PRN_8bit_GEN() % 50);
   Timer_T0_10mS_delay_x_m(PRN);
 
-if (eeprom_read_byte((uint8_t*)(0x3FC))%2)
+if (eeprom_read_byte((uint8_t*)(0x3F8))%2)
 {LED_1_on;}
 else {LED_2_on;}
 
-eeprom_write_byte((uint8_t*)(0x3FC), 
-(eeprom_read_byte((uint8_t*)(0x3FC))+1));
+eeprom_write_byte((uint8_t*)(0x3F8), 
+(eeprom_read_byte((uint8_t*)(0x3F8))+1));
 SW_reset;}
   
