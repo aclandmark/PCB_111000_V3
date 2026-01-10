0a7f3cd9f90e33d20b91f50d9c04cf2a38dce9bc
aclandmark <mark.osborne15@btinternet.com>
2026-01-10 12:00:10 +0000

    Create 1_Segment_driver.ino

---
+++ 1_Arduino Projects/4_Display_Driver_Basic/1_Segment_driver/1_Segment_driver.ino
@@ -0,0 +1,22 @@
+
+/*
+ Enables users to drive the display manually
+ This indicates that there is not necessarily any connection between 
+ the display and an actual number.
+ */
+
+#include "Segment_driver_header.h"
+
+
+#define message_1 "\r\nPress key a to g (x for next digit).\r\n\
+Press 'z' to clear the display.\r\n"
+#define message_2 "?\r\n"
+
+
+//Type main routine here
+
+
+
+
+
+/********************************************************************************************************************/
